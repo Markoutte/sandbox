09162ee3fed05a9940de30ec5146f5cb7402bb33
Maksim Pelevin <maks.pelevin@gmail.com>
2015-10-17 10:54:02 +0300

    Forgotten files

--- src/main/java/me/markoutte/sandbox/stupid/RegexpWithBackSlash.java
+++ src/main/java/me/markoutte/sandbox/stupid/RegexpWithBackSlash.java
@@ -1,4 +1,10 @@
 package me.markoutte.sandbox.stupid;
 
 public class RegexpWithBackSlash {
+
+    public static void main(String[] args) {
+        System.out.println(".".replaceAll(".", "/"));
+        System.out.println(".".replaceAll(".", "\\")); // <-- throws IllegalArgumentException
+    }
+
 }
