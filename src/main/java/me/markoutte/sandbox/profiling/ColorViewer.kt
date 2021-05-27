f2b335ec44bb5253114829a8dc9bea2452e472cd
Maksim Pelevin <maks.pelevin@gmail.com>
2021-05-27 23:04:22 +0300

    Rework examples for the talk about profiler

--- src/main/java/me/markoutte/sandbox/profiling/ColorViewer.kt
+++ src/main/java/me/markoutte/sandbox/profiling/ColorViewer.kt
@@ -1,110 +1,79 @@
 package me.markoutte.sandbox.profiling
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import me.markoutte.sandbox.swing.SwingExample
 import me.markoutte.sandbox.swing.monsters.EventCounter
-import org.openjdk.jmh.infra.Blackhole
-import java.awt.*
+import java.awt.Color
+import java.awt.Graphics
+import java.awt.Graphics2D
 import java.awt.geom.Rectangle2D
-import javax.swing.*
-import kotlin.properties.Delegates
-
-class MyPanel : JComponent() {
-
-    var hue : Int by Delegates.observable(0) { _, _, _ ->
-        repaint()
-    }
-
-    private val colors: Array<MyColor> = Array(256 * 256) { i ->
-        MyColor {
-            Blackhole.consumeCPU(600)
-            Color.getHSBColor(
-                hue / 256f,
-                (i % 256) / 256f,
-                (i / 256) / 256f
-            )
-        }
-    }
-
-    class MyColor(val producer: () -> Color) : Color(0) {
-        override fun getRGB(): Int {
-            return producer().rgb
-        }
-    }
-
-//    private var cached: Array<Color>? = null
-//
-//    var isCached: Boolean = false
-//        set(value) {
-//            field = value
-//            cached = if (value) Array(colors.size) { i ->
-//                Color(colors[i].rgb)
-//            } else null
-//        }
-//        get() = cached != null
-
-    override fun paintComponent(g: Graphics) {
-        val threshold = 5
-        val width = width * threshold / 256f
-        val height = height * threshold / 256f
-        for (i in 0..255 step threshold) {
-            for (j in 0..255 step threshold) {
-                g.color = colors[j * 256 + i]//(cached ?: colors)[j * 256 + i]
-                (g as Graphics2D).fill(
-                    Rectangle2D.Float(
-                        width * i / threshold,
-                        height * (255 - j) / threshold,
-                        width,
-                        height
-                    )
-                )
-            }
-        }
-
-        counter.update()
-    }
-
-    private val counter = EventCounter(100000)
-
-    val count: Int
-        get() = counter.size()
-}
+import java.util.*
+import javax.swing.JComponent
 
 fun main() {
-    val panel = MyPanel()
-    val label = JLabel()
-    SwingExample.inFrame("Theme Colors", JPanel().apply {
-        layout = BorderLayout()
-        panel.layout = null
-        label.isOpaque = true
-        label.background = Color.WHITE
-        panel.add(label)
-        val slider = JSlider(0, 255, 0)
-        slider.addChangeListener {
-            panel.hue = slider.value
-        }
-//        val checkbox = JCheckBox("Use cache")
-//        checkbox.addActionListener {
-//            panel.isCached = checkbox.isSelected
-//        }
-        add(panel, BorderLayout.CENTER)
-        add(JPanel().apply {
-            layout = BoxLayout(this, BoxLayout.LINE_AXIS)
-//            add(checkbox)
-            add(slider)
-        }, BorderLayout.SOUTH)
-    }).isVisible = true
-    GlobalScope.launch {
-        @Suppress("BlockingMethodInNonBlockingContext")
-        while (true) {
-            delay(1)
-            SwingUtilities.invokeAndWait {
-                panel.paintImmediately(panel.visibleRect)
+    SwingExample.inFrame("Color of the Year", MyPanel()).isVisible = true
+}
+
+class MyPanel : JComponent() {
+    override fun paintComponent(g: Graphics) {
+        g as Graphics2D
+        val width = width / 256f
+        val height = height / 256f
+        for (i in 0..255) {
+            for (j in 0..255) {
+                g.color = MyColor
+                    .fromProperties("color")
+                    .withSaturation(i / 256f)
+                    .withBrightness(j / 256f)
+                val rect = Rectangle2D.Float(width * i, height * (255 - j), width, height)
+                g.fill(rect)
             }
-            label.text = "Count: ${panel.count}"
-            label.bounds = Rectangle(5, 5, label.preferredSize.width, label.preferredSize.height)
         }
     }
 }
+
+class MyColor(val producer: () -> Color) : Color(0) {
+
+    override fun getRGB(): Int {
+        return producer().rgb
+    }
+
+    fun withSaturation(s: Float) = MyColor {
+        withHSB { h, _, b ->
+            getHSBColor(h, s, b)
+        }
+    }
+
+    fun withBrightness(b: Float) = MyColor {
+        withHSB { h, s, _ ->
+            getHSBColor(h, s, b)
+        }
+    }
+
+    private fun <R> withHSB(block: (hue: Float, saturation: Float, brightness: Float) -> R): R {
+        val hsb = FloatArray(3)
+        RGBtoHSB(red, green, blue, hsb)
+        return block(hsb[0], hsb[1], hsb[2])
+    }
+
+    companion object {
+        fun fromProperties(key: String) = MyColor {
+            parseColor(property(key))
+        }
+
+        @Suppress("MemberVisibilityCanBePrivate")
+        fun parseColor(hex: String) = MyColor {
+            Color(Integer.parseInt(hex, 16))
+        }
+    }
+}
+
+
+private val properties = Properties().apply {
+    put("color", "F5DF4D")
+}
+
+@Suppress("SameParameterValue")
+private fun property(key: String) = properties.getProperty(key)
+
+@Suppress("unused")
+private val counter = EventCounter(100000)
--- src/main/java/me/markoutte/sandbox/profiling/CountEvents.java
+++ src/main/java/me/markoutte/sandbox/profiling/CountEvents.java
@@ -1,8 +1,11 @@
 package me.markoutte.sandbox.profiling;
 
-import org.openjdk.jmh.infra.Blackhole;
-
-import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -10,33 +13,89 @@ import java.util.concurrent.TimeUnit;
  */
 public class CountEvents {
 
-    public static int update(Deque<Long> events, long nanos, long interval) {
-        events.add(nanos);
-        // we can remove values like this
-//        events.removeIf(aTime -> aTime < nanos - interval);
-
-        //noinspection ConstantConditions
-        while (events.peekFirst() < nanos - interval) {
-            events.removeFirst();
-        }
-        return events.size();
-    }
-
-    public static void main(String[] args) {
+    // enable -XX:TieredStopAtLevel=0
+    public static void main(String[] args) throws IOException {
+        var path = Paths.get("dir");
         long start = System.nanoTime();
         int total = 100_000;
         long interval = TimeUnit.MILLISECONDS.toNanos(100);
         int[] count = new int[total];
-        // usually ArrayList is used, but ArrayDeque is pretty good
-        Deque<Long> collection = new ArrayDeque<>();
+        Counter fps = new DequeCounter(interval);
         for (int counter = 0; counter < count.length; counter++) {
-            count[counter] = update(collection, System.nanoTime(), interval);
-            // https://shipilev.net/blog/2014/nanotrusting-nanotime/
-            Blackhole.consumeCPU(1000);
+            count[counter] = fps.update();
+//            if (!Files.exists(path)) {
+                Files.createDirectories(path);
+//            }
         }
         long spent = System.nanoTime() - start;
         //noinspection OptionalGetWithoutIsPresent
         System.out.println("Average count: " + (int) (Arrays.stream(count).average().getAsDouble()) + " op");
         System.out.println("Spent time: " + TimeUnit.NANOSECONDS.toMillis(spent) + " ms");
+    }
+
+    private interface Counter {
+        int update();
+    }
+
+    private static class DequeCounter implements Counter {
+
+        // usually ArrayList is used, but ArrayDeque is pretty good
+        private final Deque<Long> collection = new ArrayDeque<>();
+        private final long interval;
+
+        private DequeCounter(long interval) {
+            this.interval = interval;
+        }
+
+        @Override
+        public int update() {
+            return update(collection, System.nanoTime(), interval);
+        }
+
+        @SuppressWarnings("CommentedOutCode")
+        public static int update(Deque<Long> events, long nanos, long interval) {
+            events.add(nanos);
+            // we can remove values like this
+            events.removeIf(aTime -> aTime < nanos - interval);
+
+//            while (events.peekFirst() < nanos - interval) {
+//                events.removeFirst();
+//            }
+            return events.size();
+        }
+    }
+
+    @SuppressWarnings("unused")
+    private static class SimpleCounter implements Counter {
+
+        private long times, prev = -1;
+        private int counter, frames;
+        private boolean flashed;
+        private final long interval;
+
+        private SimpleCounter(long interval) {
+            this.interval = interval;
+        }
+
+        public int update() {
+            var stop = System.nanoTime();
+            if (prev < 0) {
+                prev = stop;
+            }
+            times += stop - prev;
+            counter++;
+            if (times > interval) {
+                frames = counter;
+                counter = 0;
+                times = 0;
+                flashed = true;
+            }
+            if (!flashed) {
+                frames = counter;
+            }
+            prev = stop;
+            return frames;
+        }
+
     }
 }
